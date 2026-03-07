5b13d1b2a3e9323b197918b50f8be26b66ca132e
sarthakmadhamshettiwar-uc <sarthakmadhamshettiwar@urbancompany.com>
2026-03-08 01:13:57 +0530

    use localStorage for persistence and debouncing to avoid sending frequenty updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,57 @@
 import './App.css';
-import { useState, useMemo } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
-// importing data
 import products from "./data/products";
 import Product from "./components/card/products/product";
 import CartSummary from "./components/cart/CartSummary";
 import Header from "./components/header/Header";
-import type { ProductsQuantityMap } from './types';
+import type { ProductsSelectionMap } from './types';
+
+function debounce(
+  fn: Function,
+  delay: number
+) {
+  let timeoutId: ReturnType<typeof setTimeout>;
+  return function (map: ProductsSelectionMap) {
+    clearTimeout(timeoutId);       
+    timeoutId = setTimeout(() => { 
+      fn(map);
+    }, delay);
+  };
+}
+
+
+
+function updateProductsSelectionMap(productsSelectionMap: ProductsSelectionMap){
+  // call the BE here
+  console.log("Updating selections on BE: ", productsSelectionMap);
+}
+
+
 
 function App() {
-  const initialMap = useMemo<ProductsQuantityMap>(() => {
-    const map: ProductsQuantityMap = {};
+ 
+  const [productsSelectionMap, setProductsSelectionMap] = useState<ProductsSelectionMap>(() => {
+    const cachedData = localStorage.getItem("productSelectionMap");
+    if (cachedData) {
+      return JSON.parse(cachedData);
+    }
+    
+    // Create the initial map if no cache exists
+    const map: ProductsSelectionMap = {};
     products.forEach(product => {
       map[product.id] = 0;
     });
     return map;
-  }, []);
-  
-  const [productsQuantityMap, setProductsQuantityMap] = useState<ProductsQuantityMap>(initialMap);
+  });
+
+  const debouncedUpdate = useRef(debounce(updateProductsSelectionMap, 3000));
+
+  useEffect(() => {
+    // every time productsSelectMap is updated: update the localStorage
+    localStorage.setItem("productSelectionMap", JSON.stringify(productsSelectionMap));
+    debouncedUpdate.current(productsSelectionMap);
+  }, [productsSelectionMap]);
 
   return (
     <div className="app-wrapper">
